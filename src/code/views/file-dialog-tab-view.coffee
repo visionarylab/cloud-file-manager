visionarylab/cloud-file-manager

commit 707b1cf0de8d3245629ff3b56a0a1aa54a0e545a

Correctly set parent folder metadata
If a user named their doc using the title input field, then clicked
Save and selected a folder, the folder selection would not be used.

If the user types in a filename in the Save dialog, the `confirm`
function correctly sets the appropriate folder, but this does not happen
unless the filename has been changed.

The `getStateForFolder` function sets the folder in the state, but this
is never passed into the metadata. This commit fixes that.

[#120377399]

src/code/views/file-dialog-tab-view.coffee
@@ -93,6 +93,8 @@ FileDialogTab = React.createClass
 
   getStateForFolder: (folder) ->
     metadata = if @isOpen() then @state?.metadata or null else @props.client.state.metadata
+    metadata.parent = folder
+
     folder: folder
     metadata: metadata
     list: []

commit 510b9334871ff455f77aed2f024d95759505b307

Fix bug caused by 707b1cf0d

src/code/views/file-dialog-tab-view.coffee
@@ -93,7 +93,7 @@ FileDialogTab = React.createClass
 
   getStateForFolder: (folder) ->
     metadata = if @isOpen() then @state?.metadata or null else @props.client.state.metadata
-    metadata.parent = folder
+    metadata?.parent = folder
 
     folder: folder
     metadata: metadata