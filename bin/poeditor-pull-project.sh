f50b4871ba77a344392817d601b2b5851c56cb77
Kirk Swenson <kswenson@concord.org>
2017-09-06 14:37:36 -0700

    Update translations - install push/pull scripts - add Hebrew, Turkish translations

---
+++ bin/poeditor-pull-project.sh
@@ -0,0 +1,32 @@
+PROJECT_ID=125177
+OUTPUT_DIR=src/code/utils/lang
+LANGUAGES=("he" "tr" "zh-TW")
+
+# argument processing from https://stackoverflow.com/a/14203146
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -a|--api_token)
+    API_TOKEN="$2"
+    shift # past argument
+    ;;
+    -o|--output_dir)
+    OUTPUT_DIR="$2"
+    shift # past argument
+    ;;
+esac
+shift # past argument or value
+done
+
+for LANGUAGE in "${LANGUAGES[@]}"
+do
+    PULLARGS="-p $PROJECT_ID -l $LANGUAGE -o $OUTPUT_DIR -a $API_TOKEN"
+    # echo "PULLARGS=$PULLARGS"
+    ./bin/poeditor-pull.sh $PULLARGS
+done
+
+# copy English strings, stripping comments
+./node_modules/.bin/strip-json-comments src/code/utils/lang/en-US-master.json \
+                                      > src/code/utils/lang/en-US.json
