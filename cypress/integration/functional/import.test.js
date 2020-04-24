e24b616b6eb77344b41fdf8f44aea7541a4e72fa
eireland <eireland@concord.org>
2020-04-24 14:56:55 -0700

    Added import test scripts Added HTML changes to use in test verification

--- cypress/integration/functional/import.test.js
+++ cypress/integration/functional/import.test.js
@@ -5,20 +5,20 @@ const ext = '.txt';
 const cfm = new CfmObject;
 const docArea = new DocumentObject;
 
-beforeEach(()=>{
+before(()=>{
     cy.visit('/examples/all-providers.html')
 })
 context('Import from different providers',()=>{
     describe('import from Local File',()=>{
         it('verify import from Local File',()=>{
             cfm.importFromLocal('Local_File_Save')
-            docArea.getTextArea().should('contain',"Imported data")
+            docArea.getTextArea().should('contain','name": "Local_File_Save",')
         })
     })
     describe('import from URL',()=>{
         it('verify import from URL',()=>{
             var importURL ='https://data.cityofnewyork.us/api/views/25th-nujf/rows.csv'
-            var text = 'Imported data: {"url": "https://data.cityofnewyork.us/api/views/25th-nujf/rows.csv","via": "select"}'
+            var text = '"url": "https://data.cityofnewyork.us/api/views/25th-nujf/rows.csv",'
             cfm.importFromURL(importURL)
             docArea.getTextArea().should('contain',text)
         })
