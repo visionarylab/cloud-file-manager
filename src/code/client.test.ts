1dd70c8da624dc034d0f8441d75d3e4bf910c591
Noah Paessel <knowuh@gmail.com>
2020-04-29 16:09:38 -0400

    Add another client test
    * Add Jest test framework
    * Add a passing test
    * Add Jest to Travis CI script
    
    [#172254686]
    https://www.pivotaltracker.com/n/projects/2441249/stories/172254686

--- src/code/client.test.ts
+++ src/code/client.test.ts
@@ -1,8 +1,24 @@
 import {CloudFileManagerClient, CloudFileManagerClientEvent} from "./client"
+import LocalStorageProvider from "./providers/local-file-provider"
+import ReadOnlyProvider from "./providers/readonly-provider"
 
-test('Client id increments with each event registered', () => {
+test('ClientEvent id increments with each event registered', () => {
   const clientEvent = new CloudFileManagerClientEvent({type: "any", data: {}, state: {}})
   expect(clientEvent.id).toBe(1)
   const clientEvent2 = new CloudFileManagerClientEvent({type: "any", data: {}, state: {}})
   expect(clientEvent2.id).toBe(2)
+})
+
+
+test('Client creates a list of its providers', () => {
+  const client = new CloudFileManagerClient()
+  const options = {
+    providers: [
+      "localStorage",
+      "localFile",
+      "lara"
+    ]
+  }
+  client.setAppOptions(options)
+  expect(Object.keys(client.providers).length).toBe(3)
 })
