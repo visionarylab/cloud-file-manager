8e0e8a9929959263ba8dff51769c210311b14eb2
eireland <eireland@concord.org>
2020-05-01 13:59:25 -0700

    Added button test, but restore is still missing because of debugger comment in main code

--- cypress/integration/functional/save-and-restore.test.js
+++ cypress/integration/functional/save-and-restore.test.js
@@ -118,19 +118,27 @@ context('Save and Restore from different providers from CFM menu',()=>{
         })
     })
 }) 
-context('Save and Restore from different providers from buttons',()=>{
-    describe('from local storage',()=>{
-        var text = "Save and restore me from buttons",
+context('Save and Restore from buttons',()=>{
+    describe('Save',function(){
+        //Only verify that dialog comes up since actual Save is tested from CFM menu
+        it('verify Save buttton opens Save dialog', function(){
+            docArea.getSaveButton().click()
+            cfm.getModalDialogTitle().should('contain','Save')
+            cy.get('.dialogTab.localfileSave button').contains('Cancel').click()
+        })
+    })
+    describe('Save As',()=>{
+        var text = "Save as and restore me from buttons",
             title = "button saved document"
         var titleURI = encodeURIComponent(title)
 
-        it('save a doc',()=>{
+        it('save as a doc', function(){ //Save as is not accessible from menu so testing it here
             docArea.getTextArea().type(text+"{enter}")
-            cfm.getDocumentTitle().click().find('input').type(title+"{enter}");
-            docArea.getSaveButton().click();
-            cfm.getTab('Local Storage').click();
+            cfm.getDocumentTitle().click().find('input').type(title+"{enter}")
+            docArea.getSaveAsButton().click()
+            cfm.getTab('Local Storage').click()
             cfm.getDocumentSaveFilenameField().should('have.value',title)
-            cfm.getSaveButton().click();
+            cfm.getSaveButton().click()
             docArea.getTextArea('{clear}')
             cy.saveLocalStorageCache()
         })
@@ -152,74 +160,28 @@ context('Save and Restore from different providers from buttons',()=>{
             docArea.getTextArea().should('contain',text)
         })
     })
-    describe('from local file',()=>{
-        var text = "saving to Local File from button",
-            title = "Button_Local_File_Save"
-        before(()=>{
-            cy.visit('/examples/all-providers.html')
-        })    
-        it('save a doc',()=>{
+    describe('Export Separate File',()=>{
+        var text = "Export and restore me from buttons",
+            title = "button export document"
+        var titleURI = encodeURIComponent(title)
+
+        it('Export a doc', function(){ //Save as is not accessible from menu so testing it here
             docArea.getTextArea().type(text+"{enter}")
-            cfm.getDocumentTitle().click().find('input').type(title+"{enter}");
-            cfm.openCFMMenu();
-            cfm.selectCFMMenuItem('Save')
-            cfm.getTab('Local File').click();
+            cfm.getDocumentTitle().click().find('input').clear().type(title+"{enter}");
+            docArea.getExportButton().click();
+            cfm.getTab('Local File').click();//Exporting to Local Storage does not work
             cfm.getDocumentSaveFilenameField().should('have.value',title)
-            cy.get('.modal-dialog-workspace .dialogTab.localFileSave input').click().clear().type(title);
             cy.get('.buttons a').contains('Download').click();
+            docArea.getTextArea('{clear}')
         })
-        it('verify file name is appended to URL',()=>{
-            cy.url().should('not.contain','#file=')
-        })
-        it('verify status info',()=>{
-            cfm.getFileStatusInfo().should('contain', 'All changes saved to Local File')
-        })
-        it('verify restore',()=>{
-            //Restore is coming from fixture file. 
-            //Ideally, needs to come from Downloads folder to verify the save.
+        it.skip('verify restore',()=>{ //there's a WIP in main code line that's causing this to fail
+            //Cypress clears local storage between tests so have to restore within the same test
             cy.visit('/examples/all-providers.html')
-            cfm.openCFMMenu();
-            cfm.selectCFMMenuItem('Open ...')
-            // cfm.openLocalDoc(dir+title);
-            cfm.openLocalDoc(title+'.txt')
+            docArea.getOpenLocalFileButton()
+            cy.iframeUploadFile('[data-test="open-local-button"]',filename,'text')
             cy.wait(2000)
             cfm.getDocumentTitle().should('contain',title);
             docArea.getTextArea().should('contain',text)
         })
-    })
-    describe.skip('from Google Docs',()=>{ //Need to figure out how the oauth works
-        var text = "saving to Google Docs",
-            title = "Google_Save"
-        before(()=>{
-            cy.visit('/examples/all-providers.html')
-        })    
-        it('save a doc',()=>{
-            docArea.getTextArea().type(text+"{enter}")
-            cfm.getDocumentTitle().click().find('input').type(title+"{enter}");
-            cfm.openCFMMenu();
-            cfm.selectCFMMenuItem('Save')
-            cfm.getTab('Google Drive').click();
-
-            cfm.getDocumentSaveFilenameField().should('have.value',title)
-            cfm.getSaveButton().click()
-        })
-        it('verify file name is appended to URL',()=>{
-            cy.url().should('contain','#file=')
-        })
-        it('verify status info',()=>{
-            cfm.getFileStatusInfo().should('contain', 'All changes saved to Google Docs')
-        })
-        it('verify restore',()=>{
-            //Restore is coming from fixture file. 
-            //Ideally, needs to come from Downloads folder to verify the save.
-            cy.visit('/examples/all-providers.html')
-            cfm.openCFMMenu();
-            cfm.selectCFMMenuItem('Open ...')
-            // cfm.openLocalDoc(dir+title);
-            this.getTab('Google Drive').click();
-            cy.wait(2000)
-            cy.get('.filelist .selectable').contains(filename).click({force:true});
-            this.getOpenDialogOpenButton();
-        })
-    })
+    })    
 })   
