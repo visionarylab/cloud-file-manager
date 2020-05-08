f7b227f36e0cadb22f623d166627a83b42fab87a
Noah Paessel <knowuh@gmail.com>
2020-05-08 10:41:38 -0400

    Add some tests for S3-share-provider
    [#172225454]
    https://www.pivotaltracker.com/story/show/172225454

---
+++ src/code/providers/s3-share-provider.test.ts
@@ -0,0 +1,59 @@
+
+import { ImportMock } from 'ts-mock-imports'
+import S3ShareProvider from "./s3-share-provider"
+import * as helperModule from './s3-share-provider-token-service-helper'
+import * as Client from "../client"
+import { CloudContent, CloudMetadata, ICloudMetaDataSpec } from "./provider-interface"
+
+const clientMockManager = ImportMock.mockClass(Client, 'CloudFileManagerClient')
+const client = clientMockManager.getMockInstance()
+
+const publicUrl = 'publicUrl'
+const resourceId = 'resourceId'
+const readWriteToken = 'RWToken'
+
+
+describe("S3ShareProvider", () => {
+  const provider = new S3ShareProvider(client)
+  const masterContent = new CloudContent({content: "test"}, "huh")
+  const sharedContent = new CloudContent({content: "test 2"}, "huh")
+  const metadata = new CloudMetadata({filename: "test"})
+
+  const createResult = new Promise( (resolve) => {
+    const data = { publicUrl, resourceId, readWriteToken }
+    resolve (data)
+  })
+
+  const updateResult = createResult
+  const create = ImportMock.mockFunction(helperModule, "createFile", createResult)
+  const upate = ImportMock.mockFunction(helperModule, 'updateFile', updateResult)
+  describe("share", () => {
+    describe("When not previously shared", () => {
+      it("Should return a new ReadWrite token", done => {
+        const callback = (error:any, data: any) => {
+          try {
+            // Don't expect an error:
+            expect(error).toBeNull()
+            // Expect the Read & write token in the callback:
+            expect(data).toBe(readWriteToken)
+            const originalDoc = masterContent.getContent()
+            // expect the masterConent to now have a documentID:
+            expect(originalDoc?.sharedDocumentId).toBe(resourceId)
+            // expect the masterConent to now have a readWriteToken:
+            expect(originalDoc?.accessKeys?.readWrite).toBe(readWriteToken)
+            done()
+          }
+          catch(e) {
+            done(e)
+          }
+        }
+        const result = provider.share(true, masterContent, sharedContent, metadata, callback)
+        // the share method doesn't return anything ...
+        expect(result).toBeUndefined()
+      })
+    })
+
+  })
+})
+
+
